7d5c4232673963ff18b0b368ebde854e5cfc63e2
Harsh Yadav <134506696+harshy1620@users.noreply.github.com>
2023-07-10 15:04:16 +0530

    Update mongoose.js
    added url(0.0.0.0)

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connect to database
-mongoose.connect('mongodb://127.0.0.1:27017/Habit_Tracker_App',{
+mongoose.connect('mongodb://0.0.0.0/Habit_Tracker_App',{
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
